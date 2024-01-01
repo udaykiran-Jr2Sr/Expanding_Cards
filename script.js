bcc575f9f48a1d008c7566caece0c90741735632
UdayKiran <nimmanaudaykiran@gmail.com>
2024-01-01 09:18:18 +0530

    clicked  will enlarge the image
    when the card got clicked it will enlarge
    should also add the descriptions when the card got clicked.

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-num_images = document.querySelectorAll('.container div').length;
-for (var i = 0; i< num_images; i++) {
-    document.querySelectorAll(".container div")[i].addEventListener("click", enlarge)
-};
+num_images = document.querySelectorAll(".container div").length;
+for (var i = 0; i < num_images; i++) {
+  document
+    .querySelectorAll(".container div")
+    [i].addEventListener("click", enlarge);
+}
 
-function enlarge(){
+function enlarge() {
+  for (var i = 0; i < num_images; i++) {
+    document.querySelectorAll(".container div")[i].classList.remove("active");
+  }
 
-    for (var i = 0; i< num_images; i++) {
-        document.querySelectorAll(".container div")[i].classList.remove("active");
-    };
-    
-    this.classList.add("active");
-    this.querySelector("p");
+  this.classList.add("active");
+  this.querySelector(".container div p")["visibility"] = "visible";
 }
 
 
-
-
-
-
-
+//To do get the discriptions visible when the card got pressed down and add some animation to the text
+//and the less numbers of cards are shown in smaller screens
