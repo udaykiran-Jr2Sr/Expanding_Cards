6cba4367dad15788bbda884a4ae347b4abf85032
UdayKiran <nimmanaudaykiran@gmail.com>
2024-01-01 08:38:43 +0530

    Updated the enlarging of cards

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-console.log("Starting");
+num_images = document.querySelectorAll('.container div').length;
+for (var i = 0; i< num_images; i++) {
+    document.querySelectorAll(".container div")[i].addEventListener("click", enlarge)
+};
 
+function enlarge(){
+
+    for (var i = 0; i< num_images; i++) {
+        document.querySelectorAll(".container div")[i].classList.remove("active");
+    };
+    
+    this.classList.add("active");
+    this.querySelector("p");
+}
 
 
 
